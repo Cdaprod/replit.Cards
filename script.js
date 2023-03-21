419b2f3b41131b590f0fc72e212f49e62f03e757
David <104325852+Cdaprod@users.noreply.github.com>
2023-03-21 23:12:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const card = document.querySelector('card');
+
+constructor() {
+      document.collection.map((card) => {
+        1. { name: David};
+        2. { name: Kayson};
+        3. { name: Kayne};
+        4. { name: Kole};
+      });
+   }
+
+const card();
+
